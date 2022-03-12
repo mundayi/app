a81f6bac92bef6e0ffb407f156789f71c76bf690
andrew <mundayiandre2@gmail.com>
2022-03-12 15:14:21 +0200

    My Pollster app, first commit

---
+++ polls/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.forms.forms import Form
+from django.forms import fields, models, widgets
+from django.db import models
+from django.forms import ModelForm
+from django.db.models import fields
+from django.urls.conf import include
+from polls.models import Question, Choice
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model =Question
+        fields = ['question_text']
+
+
+class ChoiceForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        fields = ['question','choice_text']
